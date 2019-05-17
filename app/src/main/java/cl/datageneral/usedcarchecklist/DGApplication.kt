77d332c3b073ba8033396cad3bab75d722c40aaa
Pablo Molina <edsacm@gmail.com>
2019-05-17 16:37:15 -0400

    add support to dagger2

--- app/src/main/java/cl/datageneral/usedcarchecklist/DGApplication.kt
+++ app/src/main/java/cl/datageneral/usedcarchecklist/DGApplication.kt
@@ -1,15 +1,25 @@
 package cl.datageneral.usedcarchecklist
 
-import android.app.Application
+import cl.datageneral.usedcarchecklist.di.component.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
 import io.realm.Realm
 
 
 /**
  * Created by Pablo Molina on 16-11-2018. s.pablo.molina@gmail.com
  */
-class DGApplication : Application() {
+class DGApplication : DaggerApplication() {
 	override fun onCreate() {
 		super.onCreate()
 		Realm.init(this)
+	}
+
+	override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+		val appComponent = DaggerAppComponent.builder()
+				.application(this)
+				.build()
+		appComponent.inject(this)
+		return appComponent
 	}
 }
