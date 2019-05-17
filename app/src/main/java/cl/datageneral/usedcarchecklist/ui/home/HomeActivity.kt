e9165bb2ba034b9a17ed106429a19a10b73f8f04
Pablo Molina <edsacm@gmail.com>
2019-05-17 16:39:03 -0400

    error correction

--- app/src/main/java/cl/datageneral/usedcarchecklist/ui/home/HomeActivity.kt
+++ app/src/main/java/cl/datageneral/usedcarchecklist/ui/home/HomeActivity.kt
@@ -1,10 +1,14 @@
 package cl.datageneral.usedcarchecklist.ui.home
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import cl.datageneral.usedcarchecklist.R
+import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : DaggerAppCompatActivity(), HomeContract.View {
+
+	@Inject
+	lateinit var presenter: HomePresenter<HomeContract.View>
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
